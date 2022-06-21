79f378399c421ed155696eb24db67707f7e75308
Adedapo Oluwatosin <janetoluwatosin2015@gmail.com>
2022-06-22 00:56:28 +0100

    upload index.html and mine.js

---
+++ mine.js
@@ -0,0 +1,11 @@
+    var firstName= 'Oluwatosin';
+    var middleName= 'Janet';
+    var lastName= 'Adedapo';
+    var height= "5'6ft";
+    var country= "Nigeria";
+
+    name= firstName + ' ' + middleName + ' ' + lastName;
+ 
+    document.write('Name:' + ' '+ name + '<br/>');
+    document.write('Height:' + ' '+ height + '<br/>');
+    document.write('Country:' + ' '+ country + '<br/>');
