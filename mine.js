e5d4871d82985d542c8288099db7c853467997f6
Adedapo Oluwatosin <janetoluwatosin2015@gmail.com>
2022-06-22 21:41:49 +0100

    added const fullName

--- mine.js
+++ mine.js
@@ -4,7 +4,8 @@
     var height= "5'6ft";
     var country= "Nigeria";
 
-    name= firstName + ' ' + middleName + ' ' + lastName;
+    
+    const fullName= firstName + ' ' + middleName + ' ' + lastName;
  
     document.write('Name:' + ' '+ name + '<br/>');
     document.write('Height:' + ' '+ height + '<br/>');
