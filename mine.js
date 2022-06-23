d875b480176b29a409e719a0d81b04beb34b9340
Adedapo Oluwatosin <janetoluwatosin2015@gmail.com>
2022-06-23 21:23:00 +0100

    changedbto alert

--- mine.js
+++ mine.js
@@ -1,12 +1,3 @@
-    var firstName= 'Oluwatosin';
-    var middleName= 'Janet';
-    var lastName= 'Adedapo';
-    var height= "5'7ft";
-    var country= "Nigeria";
-
-    
-    const name= firstName + ' ' + middleName + ' ' + lastName;
- 
-    document.write('Name:' + ' '+ name + '<br/>');
-    document.write('Height:' + ' '+ height + '<br/>');
-    document.write('Country:' + ' '+ country + '<br/>');
+alert (`My name is Adedapo Oluwatosin Janet
+I am 5'7ft Tall
+I am from Nigeria`)
